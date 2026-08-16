e6bd62c93ee4b12408d8b3c1020f50b0f6553266
nightlycacklewitch <151456716+cacklycrackle@users.noreply.github.com>
2026-08-16 22:59:19 +0800

    Jasper.java: Add simple chat loop
    To simulate a chatbot interaction, let's implement a simple chat loop
    where commands entered by the user are echoed back until the user
    exits with the command "bye".

--- src/main/java/Jasper.java
+++ src/main/java/Jasper.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Jasper {
     public static void main(String[] args) {
         String banner = """
@@ -5,18 +7,32 @@ public class Jasper {
                  _ | |  __ _   ___  _ __   ___   _ _ \s
                 | || | / _` | (_-< | '_ \\ / -_) | '_|\s
                  \\__/  \\__,_| /__/ | .__/ \\___| |_|  \s
-                                   |_|               \s""";
+                                   |_|               \s
+                """.indent(4);
         String hello = """
                 Hello! I'm Jasper.
-                What can I do for you?""";
-        String bye = "Farewell. Hope to see you again soon!";
-        String lineSeparator = "-".repeat(60);
+                What can I do for you? End session with 'bye'.
+                """.indent(4);
+        String bye = "Farewell. Hope to see you again soon!".indent(4);
+        String lineSeparator = "-".repeat(60).indent(4);
 
-        System.out.println(lineSeparator);
-        System.out.println(banner);
-        System.out.println(hello);
-        System.out.println(lineSeparator);
-        System.out.println(bye);
-        System.out.println(lineSeparator);
+        System.out.print(lineSeparator);
+        System.out.print(banner);
+        System.out.print(hello);
+
+        // Chat loop
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print(lineSeparator);
+            String line = sc.nextLine();
+            if (line.equals("bye")) {
+                break;
+            }
+            System.out.print(line.indent(4));
+        }
+        sc.close();
+
+        System.out.print(bye);
+        System.out.print(lineSeparator);
     }
 }

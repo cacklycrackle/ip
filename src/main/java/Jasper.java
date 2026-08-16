d55edb3e9d6ffe14350e15b087606afa650591e8
nightlycacklewitch <151456716+cacklycrackle@users.noreply.github.com>
2026-08-16 15:32:32 +0800

    Rename Duke.java to Jasper.java and change banner accordingly

---
+++ src/main/java/Jasper.java
@@ -0,0 +1,12 @@
+public class Jasper {
+    public static void main(String[] args) {
+        String banner = """
+                _                                \s
+             _ | |  __ _   ___  _ __   ___   _ _ \s
+            | || | / _` | (_-< | '_ \\ / -_) | '_|\s
+             \\__/  \\__,_| /__/ | .__/ \\___| |_|  \s
+                               |_|               \s
+            """;
+        System.out.println(banner);
+    }
+}

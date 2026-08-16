abc0a3e671cb199fb1d8251503c3a3a2f6e01d49
nightlycacklewitch <151456716+cacklycrackle@users.noreply.github.com>
2026-08-16 15:54:53 +0800

    Jasper.java: Add greeting and exit, and fix indentation

--- src/main/java/Jasper.java
+++ src/main/java/Jasper.java
@@ -1,12 +1,22 @@
 public class Jasper {
     public static void main(String[] args) {
         String banner = """
-                _                                \s
-             _ | |  __ _   ___  _ __   ___   _ _ \s
-            | || | / _` | (_-< | '_ \\ / -_) | '_|\s
-             \\__/  \\__,_| /__/ | .__/ \\___| |_|  \s
-                               |_|               \s
-            """;
+                    _                                \s
+                 _ | |  __ _   ___  _ __   ___   _ _ \s
+                | || | / _` | (_-< | '_ \\ / -_) | '_|\s
+                 \\__/  \\__,_| /__/ | .__/ \\___| |_|  \s
+                                   |_|               \s""";
+        String hello = """
+                Hello! I'm Jasper.
+                What can I do for you?""";
+        String bye = "Farewell. Hope to see you again soon!";
+        String lineSeparator = "-".repeat(60);
+
+        System.out.println(lineSeparator);
         System.out.println(banner);
+        System.out.println(hello);
+        System.out.println(lineSeparator);
+        System.out.println(bye);
+        System.out.println(lineSeparator);
     }
 }

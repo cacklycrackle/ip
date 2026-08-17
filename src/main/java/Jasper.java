5181eac48c017acc94b43a4672efb663076161c5
nightlycacklewitch <151456716+cacklycrackle@users.noreply.github.com>
2026-08-17 14:59:09 +0800

    Jasper.java: Refactor chat loop to use Tasks and switch expressions
    To leverage polymorphism, let's store different types of tasks as
    instances of their parent Task class. Doing so will only expose
    common functionality that the main chat loop needs to execute user
    commands, reducing code duplication.
    
    When parsing commands to execute, long and nested if statements make it
    harder to read and maintain. Let's parse commands with switch
    expressions instead to improves code quality with cleaner syntax that
    is easier to reason about.

--- src/main/java/Jasper.java
+++ src/main/java/Jasper.java
@@ -13,46 +13,70 @@ public class Jasper {
                 Hello! I'm Jasper.
                 What can I do for you? End session with 'bye'.
                 """.indent(4);
-        String bye = "Farewell. Hope to see you again soon!".indent(4);
         String lineSeparator = "-".repeat(60).indent(4);
 
+        // display welcome
         System.out.print(lineSeparator);
         System.out.print(banner);
         System.out.print(hello);
+        System.out.print(lineSeparator);
 
-        // Chat loop
+        // chat loop
         Task[] tasks = new Task[100];
         int taskIndex = 0;
         Scanner sc = new Scanner(System.in);
-        while (true) {
+        boolean quit = false;
+        while (!quit) {
+            // parse command
+            String[] tokens = sc.nextLine().strip().split("\\s+", 2);
+            String response = switch (tokens[0]) {
+                case "bye" -> {
+                    quit = true;
+                    yield "Farewell. Hope to see you again soon!";
+                }
+                case "list" -> {
+                    if (taskIndex == 0) {
+                        yield "No tasks here! Add some to track.";
+                    }
+                    StringBuilder sb = new StringBuilder("Here are your tasks:\n");
+                    for (int i = 0; i < taskIndex; ++i) {
+                        sb.append(String.format("%d.%s", i + 1, tasks[i])).append('\n');
+                    }
+                    yield sb.toString();
+                }
+                case "unmark" -> {
+                    int n = Integer.parseInt(tokens[1]) - 1; // task number in list
+                    tasks[n].markAsUndone();
+                    yield "Get to work... I've marked this task as not done yet\n  " + tasks[n];
+                }
+                case "mark" -> {
+                    int n = Integer.parseInt(tokens[1]) - 1; // task number in list
+                    tasks[n].markAsDone();
+                    yield "Alright! I've marked this task as done\n  " + tasks[n];
+                }
+                case "todo" -> {
+                    Task t = new Todo(tokens[1]);
+                    tasks[taskIndex++] = t;
+                    yield "Aye, aye. I've added this task:\n  " + t;
+                }
+                case "deadline" -> {
+                    String[] parts = tokens[1].split("\\s+/by\\s+", 2);
+                    Task t = new Deadline(parts[0].strip(), parts[1].strip());
+                    tasks[taskIndex++] = t;
+                    yield "Aye, aye. I've added this task:\n  " + t;
+                }
+                case "event" -> {
+                    String[] parts = tokens[1].split("\\s+/(from|to)\\s+");
+                    Task t = new Event(parts[0].strip(), parts[1].strip(), parts[2].strip());
+                    tasks[taskIndex++] = t;
+                    yield "Aye, aye. I've added this task:\n  " + t;
+                }
+                default -> "ERROR: unknown command";
+            };
+            // display chat response
+            System.out.print(response.indent(4));
             System.out.print(lineSeparator);
-            String line = sc.nextLine().strip();
-            if (line.equals("bye")) {
-                break;
-            } else if (line.equals("list")) {
-                for (int i = 0; i < taskIndex; ++i) {
-                    System.out.print(String.format("%d.%s", i + 1, tasks[i]).indent(4));
-                }
-            } else if (line.startsWith("unmark") || line.startsWith("mark")) {
-                String[] parts = line.split(" ");
-                int taskNum = Integer.parseInt(parts[1]) - 1;
-                String output;
-                if (parts[0].equals("mark")) {
-                    output = "Alright! I've marked this task as done";
-                    tasks[taskNum].markAsDone();
-                } else { // parts[0].equals("unmark")
-                    output = "Get to work... I've marked this tasks as not done yet";
-                    tasks[taskNum].markAsUndone();
-                }
-                System.out.print(String.format("%s:\n  %s", output, tasks[taskNum]).indent(4));
-            } else {
-                tasks[taskIndex++] = new Task(line);
-                System.out.print(String.format("added %s", line).indent(4));
-            }
         }
         sc.close();
-
-        System.out.print(bye);
-        System.out.print(lineSeparator);
     }
 }

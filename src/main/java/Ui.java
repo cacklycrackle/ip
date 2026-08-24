79a7780284cd6dd116a8d4cb5eddecb38cfaa9f0
nightlycacklewitch <151456716+cacklycrackle@users.noreply.github.com>
2026-08-25 01:03:03 +0800

    Extract class Ui.java to encapsulate UI-related functionality

---
+++ src/main/java/Ui.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Ui {
+    private static final String SEPARATOR = "-".repeat(60).indent(4);
+    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_RESET = "\u001B[0m";
+
+    private final Scanner scanner;
+
+    public Ui() {
+        scanner = new Scanner(System.in);
+    }
+
+    public void close() {
+        scanner.close();
+    }
+
+    public String readCommand() {
+        return scanner.nextLine().strip();
+    }
+
+    public void showError(String errMessage) {
+        String s = ANSI_RED + "Something is amiss... " + ANSI_RESET + errMessage;
+        showResponse(s);
+    }
+
+    public void showLine() {
+        System.out.println(SEPARATOR);
+    }
+
+    public void showResponse(String message) {
+        System.out.println(message.indent(4));
+    }
+
+    public void showWelcome() {
+        showLine();
+        System.out.println("""
+                    _                                \s
+                 _ | |  __ _   ___  _ __   ___   _ _ \s
+                | || | / _` | (_-< | '_ \\ / -_) | '_|\s
+                 \\__/  \\__,_| /__/ | .__/ \\___| |_|  \s
+                                   |_|               \s
+                """.indent(4)
+        );
+        System.out.println("""
+                Hello! I'm Jasper. Whatever shalt thou require of me today?
+                Supported commands:
+                    bye, list, unmark, mark, delete, todo, deadline, event
+                """.indent(4)
+        );
+        showLine();
+    }
+}

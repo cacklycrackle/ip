a0f22a220caa7df7daa4f10afb094a12f91de527
nightlycacklewitch <151456716+cacklycrackle@users.noreply.github.com>
2026-08-24 17:42:41 +0800

    Extract class TaskList.java to encapsulate operations on a Tasks list
    A raw list of tasks is passed around to multiple components.
    
    This exposes its internal representation and violates Single
    Responsibility Principle (SRP) by scattering repeated list operations
    across the codebase.
    
    Let's pull up behaviours by a list of Tasks into a new class
    called TaskList.
    
    This encapsulates list operations and reduces code duplication.

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class TaskList implements Iterable<Task> {
+    private final List<Task> tasks;
+
+    public TaskList(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public TaskList() {
+        this(new ArrayList<>(100));
+    }
+
+    private static void check(int index, List<Task> tasks) throws JasperException {
+        if (index < 0 || index >= tasks.size()) {
+            throw new JasperException("Task index out of range!");
+        }
+    }
+
+    public boolean isEmpty() {
+        return tasks.isEmpty();
+    }
+
+    public int size() {
+        return tasks.size();
+    }
+
+    public void add(Task t) {
+        tasks.add(t);
+    }
+
+    public Task delete(int n) throws JasperException {
+        check(n, tasks);
+        return tasks.remove(n);
+    }
+
+    public Task mark(int n) throws JasperException {
+        check(n, tasks);
+        Task t = tasks.get(n);
+        t.markAsDone();
+        return t;
+    }
+
+    public Task unmark(int n) throws JasperException {
+        check(n, tasks);
+        Task t = tasks.get(n);
+        t.markAsUndone();
+        return t;
+    }
+
+    @Override
+    public Iterator<Task> iterator() {
+        return tasks.iterator();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < tasks.size(); ++i) {
+            sb.append(i + 1).append(". ").append(tasks.get(i)).append('\n');
+        }
+        return sb.toString();
+    }
+}
